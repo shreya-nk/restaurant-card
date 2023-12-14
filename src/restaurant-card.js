b775f83ad36a89add05f52c0d554aa8ff63c3092
shreya-nk <shreya.kakumani@gmail.com>
2023-12-13 19:34:45 -0500

    add to files

--- src/restaurant-card.js
+++ src/restaurant-card.js
@@ -9,7 +9,35 @@ export class RestaurantCard extends LitElement {
     super();
     this.version = 'STARTING';
   }
+  static get styles() {
+    return css`
+    .card {
+      width: 420px;
+      margin: 0 auto;
+      border: 1px solid;
+      padding: 10px;
+      text-align: center;
+      background-color: rgb(232, 123, 123);
+    }
 
+    @media screen and (max-width: 800px) {
+    display: inline-block;
+  }
+
+@media screen and (max-width: 500px) {
+  .card {
+    max-width: 300px;
+  }
+  h2 {
+    font-size: 20px;
+  }
+  p {
+    font-size: 14px;
+  }
+}
+
+  `;
+  }
   render() {
     return html`
     <p>Welcome to the Lit tutorial!</p>
